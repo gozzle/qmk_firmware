ed2907d6d225b034f893600b1fc97c447dcc3b2f
Callum Chalk <callumchalk@google.com>
2020-07-01 17:45:17 +0100

    Add some config settings for gozzle's keymap

---
+++ keyboards/massdrop/alt/keymaps/gozzle/config.h
@@ -0,0 +1,16 @@
+# pragma once
+
+// set time held before Mod-Tab key sends mod code
+#define TAPPING_TERM 150
+
+// permissive hold: if holding down a Mod-Tap key while pressing another, it
+// acts a a mod, even within the tapping term
+#define PERMISSIVE_HOLD
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif
+
+#ifndef NO_PRINT
+#define NO_PRINT
+#endif
